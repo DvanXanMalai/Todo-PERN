1adc1679b712feef7f18a0305204f7691bb09b81
DvanXanMalai <xabilal27@gmail.com>
2025-03-13 08:05:58 +0545

    fix db connection

--- server/db.js
+++ server/db.js
@@ -6,6 +6,10 @@ const { Pool } = pkg;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host: "db.dpbuefqameottuigelnk.supabase.co",
+    port: 5432,
+    allowExitOnIdle: true
 });
 
 export default pool;
